4727f0ef92c35d3019d7ddf0c7ea2e2a786559de
Shadowctrl <shadowctrl@outlook.com>
2024-07-30 18:41:24 +0530

    render component

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,22 @@
 import Image from "next/image";
+import RenderNews from "@/components/renderNews/renderNews";
 
 export default function Home() {
-  return <main></main>;
+  const title = `How our tool analysing emotions on Twitter predicted Donald Trump win`;
+  const desc = `As the world dissects the how and why of Donald Trump’s presidential victory, pollsters are already coming under severe criticism for getting it so wrong. But a tool we created that analysed emotions towards the two candidates on Twitter accurately predicted the result. Unlike other poll`;
+  const date = "2016-11-09 17:46:24";
+
+  return (
+    <main>
+      <RenderNews
+        thumbnail={"/test.svg"}
+        title={title}
+        desc={desc}
+        date={date}
+        refLink={
+          "http://theconversation.com/how-our-tool-analysing-emotions-on-twitter-predicted-donald-trump-win-68522"
+        }
+      />
+    </main>
+  );
 }
---
+++ src/components/renderNews/renderNews.jsx
@@ -0,0 +1,31 @@
+import "./renderNews.scss";
+import Image from "next/image";
+import Link from "next/link";
+import { FaFacebook } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
+
+const RenderNews = ({ thumbnail, title, desc, date, refLink }) => {
+  return (
+    <div className="newsRender-parent">
+      <div className="news-container">
+        <div className="newsRender-left">
+          <Image src={thumbnail} width={438} height={11} />
+          <div>
+            <FaXTwitter className="text-3xl fill-yellow-400" />
+            <FaFacebook className="text-3xl fill-yellow-400" />
+          </div>
+        </div>
+        <div className="newsRender-right">
+          <h1>{title}</h1>
+          <p>{desc}</p>
+          <Link href={refLink} target="_blank">
+            <span>READ MORE</span>
+          </Link>
+          <p>{date}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default RenderNews;
