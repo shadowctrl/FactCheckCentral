5f5ec7dcb2ef6d42fa92bbcfe87904221d793f30
Shadowctrl <shadowctrl@outlook.com>
2024-08-02 09:07:45 +0530

    Cors

--- next.config.mjs
+++ next.config.mjs
@@ -11,7 +11,7 @@ const nextConfig = {
           },
           {
             key: "Access-Control-Allow-Methods",
-            value: "GET, POST",
+            value: "POST",
           },
         ],
       },
