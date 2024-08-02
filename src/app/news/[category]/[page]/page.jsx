4019624d935b089c344bafcde16c60c8b01c9862
Shadowctrl <shadowctrl@outlook.com>
2024-08-02 11:05:27 +0530

    Max page restriction

--- src/app/news/[category]/[page]/page.jsx
+++ src/app/news/[category]/[page]/page.jsx
@@ -1,31 +1,40 @@
 import RenderNews from "@/components/renderNews/renderNews";
 import { fetchNews } from "@/app/api/newsService";
 import Pagination from "@/components/pagination/pagination";
+import { redirect } from "next/navigation";
 
 const FormatDate = (value) => {
   const date = new Date(value);
   return date.toLocaleString();
 };
 const page = async ({ params }) => {
+  const maxpage = 5;
+  if (params.page > 5) redirect(`/news/${params.category}/${maxpage}`);
   const data = await fetchNews(params.category, params.page);
 
   return (
     <div>
-      {data.map((val, index) => (
-        <RenderNews
-          key={index}
-          thumbnail={
-            val.image?.thumbnail?.contentUrl
-              ? val.image.thumbnail.contentUrl
-              : "/test.svg"
-          }
-          title={val.name}
-          desc={val.description}
-          date={FormatDate(val.datePublished)}
-          refLink={val.url}
-        />
-      ))}
-      <Pagination maxPage={5} category={params.category} />
+      {data.length > 0
+        ? data.map((val, index) => (
+            <RenderNews
+              key={index}
+              thumbnail={
+                val.image?.thumbnail?.contentUrl
+                  ? val.image.thumbnail.contentUrl
+                  : "/test.svg"
+              }
+              title={val.name}
+              desc={val.description}
+              date={FormatDate(val.datePublished)}
+              refLink={val.url}
+            />
+          ))
+        : ""}
+      <Pagination
+        maxPage={maxpage}
+        category={params.category}
+        page={params.page}
+      />
     </div>
   );
 };
