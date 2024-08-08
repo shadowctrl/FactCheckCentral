b648a1efe07d54e1a98bf8b337c8dbea44399fed
Shadowctrl <shadowctrl@outlook.com>
2024-08-08 14:01:52 +0530

    Mongo Fix

--- src/app/api/voteAdd/route.js
+++ src/app/api/voteAdd/route.js
@@ -1,8 +1,16 @@
 import { MongoClient } from "mongodb";
 
-// Replace with your MongoDB connection string
 const uri = process.env.mongo_uri;
-const client = new MongoClient(uri);
+let client;
+let clientPromise;
+
+if (uri) {
+  client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  clientPromise = client.connect();
+}
 
 const dbName = process.env.mongo_db_name;
 const collectionName = process.env.mongo_collection_name;
@@ -10,20 +18,15 @@ const collectionName = process.env.mongo_collection_name;
 export const POST = async (req) => {
   const { title } = await req.json();
 
-  await client.connect();
-  const db = client.db(dbName);
+  const connection = await clientPromise;
+  const db = connection.db(dbName);
   const collection = db.collection(collectionName);
 
-  // Find the voting document with the specified title
   const voting = await collection.findOne({ title });
 
   if (voting) {
-    // If found, increment the vote
-
     await collection.updateOne({ title }, { $inc: { vote: 1 } });
   } else {
-    // If not found, insert a new document
-
     await collection.insertOne({ title, vote: 1 });
   }
 
