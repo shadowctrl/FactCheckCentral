414b1ac98cd6a4698e0bfd329afafd055728938a
Shadowctrl <shadowctrl@outlook.com>
2024-10-17 17:46:04 +0530

    Emoji on fact news

--- src/helper/factcheckFormat.jsx
+++ src/helper/factcheckFormat.jsx
@@ -1,7 +1,12 @@
 import Link from "next/link";
+
 const FactcheckFormat = (message) => {
   const lines = message.split("\n");
   const urlRegex = /(https?:\/\/[^\s]+)/g;
+
+  // Determine the emoji based on the first line
+  const emoji = lines[0].toLowerCase().startsWith("yes") ? "✅" : "❌";
+  const modifiedFirstLine = `${emoji} ${lines[0]}`;
 
   return lines.map((line, index) => (
     <div key={index}>
@@ -12,22 +17,38 @@ const FactcheckFormat = (message) => {
             : { color: "rgb(0, 0, 0,0.7)", fontWeight: 600 }
         }
       >
-        {line.split(urlRegex).map((part, i) =>
-          urlRegex.test(part) ? (
-            <Link
-              key={i}
-              href={part}
-              target="_blank"
-              rel="noopener nofollow noreferrer"
-            >
-              {part}
-            </Link>
-          ) : (
-            part
-          )
-        )}
+        {index === 0
+          ? modifiedFirstLine.split(urlRegex).map((part, i) =>
+              urlRegex.test(part) ? (
+                <Link
+                  key={i}
+                  href={part}
+                  target="_blank"
+                  rel="noopener nofollow noreferrer"
+                >
+                  {part}
+                </Link>
+              ) : (
+                part
+              )
+            )
+          : line.split(urlRegex).map((part, i) =>
+              urlRegex.test(part) ? (
+                <Link
+                  key={i}
+                  href={part}
+                  target="_blank"
+                  rel="noopener nofollow noreferrer"
+                >
+                  {part}
+                </Link>
+              ) : (
+                part
+              )
+            )}
       </p>
     </div>
   ));
 };
+
 export default FactcheckFormat;
