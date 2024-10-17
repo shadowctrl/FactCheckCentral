d9d9da7bc4f259529e89e8d8071bd93c80a0f704
Shadowctrl <shadowctrl@outlook.com>
2024-10-17 19:42:45 +0530

    Bold format

--- src/helper/factcheckFormat.jsx
+++ src/helper/factcheckFormat.jsx
@@ -3,10 +3,49 @@ import Link from "next/link";
 const FactcheckFormat = (message) => {
   const lines = message.split("\n");
   const urlRegex = /(https?:\/\/[^\s]+)/g;
+  const parenthesesRegex = /\(([^)]+)\)/g;
+
+  // Array of words/phrases to emphasize
+  const emphasizeWords = ["quick summary"];
 
   // Determine the emoji based on the first line
   const emoji = lines[0].toLowerCase().startsWith("yes") ? "✅" : "❌";
   const modifiedFirstLine = `${emoji} ${lines[0]}`;
+
+  const formatLine = (line) => {
+    // Replace specified words with strong tags
+    let formattedLine = line;
+    emphasizeWords.forEach((word) => {
+      const regex = new RegExp(`(${word})`, "gi"); // Create a regex for case-insensitive matching
+      formattedLine = formattedLine.replace(regex, "<strong>$1</strong>");
+    });
+
+    return formattedLine.split(urlRegex).flatMap((part, i) => {
+      // Replace text inside parentheses with strong tags
+      const formattedPart = part.replace(parenthesesRegex, (match, p1) => {
+        return `(<strong>${p1}</strong>)`;
+      });
+
+      // Check if the part matches the URL regex
+      if (urlRegex.test(part)) {
+        return (
+          <Link
+            key={i}
+            href={part}
+            target="_blank"
+            rel="noopener nofollow noreferrer"
+          >
+            {formattedPart}
+          </Link>
+        );
+      } else {
+        // Return a span with dangerouslySetInnerHTML for strong tags
+        return (
+          <span key={i} dangerouslySetInnerHTML={{ __html: formattedPart }} />
+        );
+      }
+    });
+  };
 
   return lines.map((line, index) => (
     <div key={index}>
@@ -17,35 +56,7 @@ const FactcheckFormat = (message) => {
             : { color: "rgb(0, 0, 0,0.7)", fontWeight: 600 }
         }
       >
-        {index === 0
-          ? modifiedFirstLine.split(urlRegex).map((part, i) =>
-              urlRegex.test(part) ? (
-                <Link
-                  key={i}
-                  href={part}
-                  target="_blank"
-                  rel="noopener nofollow noreferrer"
-                >
-                  {part}
-                </Link>
-              ) : (
-                part
-              )
-            )
-          : line.split(urlRegex).map((part, i) =>
-              urlRegex.test(part) ? (
-                <Link
-                  key={i}
-                  href={part}
-                  target="_blank"
-                  rel="noopener nofollow noreferrer"
-                >
-                  {part}
-                </Link>
-              ) : (
-                part
-              )
-            )}
+        {index === 0 ? formatLine(modifiedFirstLine) : formatLine(line)}
       </p>
     </div>
   ));
