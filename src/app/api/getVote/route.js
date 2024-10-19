57aeaa6e9c13402d35dd3f0cd71b94eca283107d
Shadowctrl <shadowctrl@outlook.com>
2024-10-19 16:10:20 +0530

    MongoDB to Postgresql

--- src/app/api/getVote/route.js
+++ src/app/api/getVote/route.js
@@ -1,43 +1,36 @@
-import { MongoClient } from "mongodb";
+import { Pool } from "pg";
 
-// Replace with your MongoDB connection string
-const uri = process.env.mongo_uri;
-let client;
-let clientPromise;
-
-const dbName = process.env.mongo_db_name;
-const collectionName = process.env.mongo_collection_name;
-
-// Create a MongoDB client and connect if not already connected
-if (!global._mongoClientPromise) {
-  client = new MongoClient(uri);
-  global._mongoClientPromise = client.connect();
-}
-clientPromise = global._mongoClientPromise;
+const pool = new Pool({
+  connectionString: process.env.postgresql_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
 
 export const POST = async (req) => {
+  const client = await pool.connect();
   try {
     const { title } = await req.json();
 
-    // Wait for the MongoDB client to connect
-    const connectedClient = await clientPromise;
-    const db = connectedClient.db(dbName);
-    const collection = db.collection(collectionName);
+    const res = await client.query(
+      "SELECT vote FROM votings WHERE title = $1",
+      [title]
+    );
 
-    // Find the voting document with the specified title
-    const vote = await collection.findOne({ title });
-
-    if (vote) {
-      // If the title is found, return the vote count
-      return new Response(JSON.stringify({ vote: vote.vote, title: title }), {
-        status: 200,
-      });
+    if (res.rows.length > 0) {
+      return new Response(
+        JSON.stringify({ vote: res.rows[0].vote, title: title }),
+        {
+          status: 200,
+        }
+      );
     } else {
-      // If the title is not found, return a 404 Not Found status
       return new Response("Title not found", { status: 404 });
     }
   } catch (error) {
     console.error("Error fetching vote:", error);
     return new Response("Internal Server Error", { status: 500 });
+  } finally {
+    client.release();
   }
 };
--- src/app/api/voteAdd/route.js
+++ src/app/api/voteAdd/route.js
@@ -1,47 +1,37 @@
-import { MongoClient } from "mongodb";
+import { Pool } from "pg";
 
-const uri = process.env.mongo_uri;
-let client;
-let clientPromise;
-
-if (uri) {
-  client = new MongoClient(uri);
-
-  // Add event listeners for logging connection events
-  client.on("open", () => {
-    console.log("MongoDB client connected");
-  });
-
-  global._mongoClientPromise = client.connect();
-  clientPromise = global._mongoClientPromise;
-}
-
-const dbName = process.env.mongo_db_name;
-const collectionName = process.env.mongo_collection_name;
+const pool = new Pool({
+  connectionString: process.env.postgresql_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
 
 export const POST = async (req) => {
+  const client = await pool.connect();
   try {
     const { title } = await req.json();
+    const res = await client.query("SELECT * FROM votings WHERE title = $1", [
+      title,
+    ]);
 
-    // Ensure the client is connected
-    const connection = await clientPromise;
-    const db = connection.db(dbName);
-    const collection = db.collection(collectionName);
-
-    // Check if the title exists in the collection
-    const voting = await collection.findOne({ title });
-
-    if (voting) {
-      // If found, increment the vote count
-      await collection.updateOne({ title }, { $inc: { vote: 1 } });
+    if (res.rows.length > 0) {
+      await client.query(
+        "UPDATE votings SET vote = vote + 1 WHERE title = $1",
+        [title]
+      );
     } else {
-      // If not found, insert a new document with initial vote count
-      await collection.insertOne({ title, vote: 1 });
+      await client.query("INSERT INTO votings (title, vote) VALUES ($1, $2)", [
+        title,
+        1,
+      ]);
     }
 
     return new Response("", { status: 200 });
   } catch (error) {
     console.error("Error processing request:", error);
     return new Response("Internal Server Error", { status: 500 });
+  } finally {
+    client.release();
   }
 };
