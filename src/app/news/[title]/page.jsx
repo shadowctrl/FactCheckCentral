1c8c93fbcb28c0c420cf2c3854222b5377a04f36
Shadowctrl <shadowctrl@outlook.com>
2024-10-24 16:03:25 +0530

    "Updated news page styles and layout, added fact-check verification icons and conditional rendering, and refactored JSX structure"

--- src/app/news/[title]/page.jsx
+++ src/app/news/[title]/page.jsx
@@ -1,10 +1,10 @@
-import RenderNews from "@/components/renderNews/renderNews";
 import Image from "next/image";
 import { FormatDate } from "@/app/topic/[category]/[page]/page";
 import FactcheckFormat from "@/helper/factcheckFormat";
 import Link from "next/link";
 import "./news.scss";
 import Share from "@/components/share/share";
+import { MdVerified, MdWarning } from "react-icons/md";
 
 const fetchSingleNews = async (title) => {
   const res = await fetch(process.env.base_url + "/api/getSingleNews", {
@@ -35,12 +35,17 @@ const getUniqueLatestNews = (latestData, currentUrl, count = 6) => {
   return uniqueLatestData.slice(0, count);
 };
 
+const checkFactVerify = async (fact) => {
+  return fact.toLowerCase().startsWith("yes") ? true : false;
+};
+
 const Page = async ({ params }) => {
   const title = decodeURIComponent(params.title);
   const data = await fetchSingleNews(title);
   const latestData = await latestNews(data.category);
-
   const uniqueLatestData = getUniqueLatestNews(latestData, data.url);
+  const newsVerified = await checkFactVerify(data.factcheck);
+
   return (
     <div className="news-parent">
       <div className="news-head">
@@ -60,14 +65,36 @@ const Page = async ({ params }) => {
             </Link>
           </h1>
 
-          <p className="news-head-para">
-            <span>{FormatDate(data.publishedat)}</span>
-            <Link
-              href={process.env.base_url + "/topic/" + data.category + "/1"}
-            >
-              <span>{data.category}</span>
-            </Link>
-          </p>
+          <div className="news-head-para">
+            {newsVerified ? (
+              <div className="news-verified-check">
+                <MdVerified
+                  className="text-blue-600 text-[22px]"
+                  title="Verified Fact"
+                />
+                <p>This news has been fact-checked</p>
+              </div>
+            ) : (
+              <div className="news-verified-check">
+                <MdWarning
+                  title="News Could not be Verfied"
+                  className=" text-orange-500"
+                />
+                <p>
+                  Warning: The authenticity of this news could not be verified.
+                </p>
+              </div>
+            )}
+
+            <div className="flex gap-2">
+              <span>{FormatDate(data.publishedat)}</span>
+              <Link
+                href={process.env.base_url + "/topic/" + data.category + "/1"}
+              >
+                <span>{data.category}</span>
+              </Link>
+            </div>
+          </div>
         </div>
       </div>
       <div className="news-description">
@@ -93,8 +120,10 @@ const Page = async ({ params }) => {
           </p>
         </div>
         <div className="fact-check-response">
-          <Share title={data.title} message={data.factcheck} />
-          {FactcheckFormat(data.factcheck)}
+          <div>
+            <Share title={data.title} message={data.factcheck} />
+            {FactcheckFormat(data.factcheck)}
+          </div>
         </div>
       </div>
       <div className="news-border" />
@@ -124,7 +153,7 @@ const Page = async ({ params }) => {
                   }
                 >
                   {" "}
-                  <h3>{value.title}</h3>
+                  <h3>{value.title.replaceAll("-", " ")}</h3>
                 </Link>{" "}
                 <p>{value.description}</p>
                 <div className="news-read-more">
